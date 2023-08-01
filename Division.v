e3cdf31833418e36e76a2430749c371ba924f674
Chetanya Katara <97398456+ChetanyaKatara@users.noreply.github.com>
2023-08-01 19:42:37 +0530

    Update Division.v

--- Division.v
+++ Division.v
@@ -11,7 +11,6 @@ wire [7:0]cmp_I2,exsel;
 wire [8:0]temp1,temp2,temp;
 wire [7:0]subE;
 wire [22:0]finalM;
-wire [8:0]exfinal;
 
 //if all the bits of exponent are 1 that means invalid input and exception is raised.
 assign redand1 = &(input1[30:23]);
@@ -34,7 +33,7 @@ assign {one_gtthan_two,subE} = input1[7:0] + cmp_I2[7:0] + 8'b00000001;
 divid div(.a({redor1,input1[22:0],24'b0}),.b({redor2,input2[22:0]}),.opt(divout[24:0]));
 
 //calculations for finding final exponent value
-assign temp1[8:0] = (one_gtthan_two) ? {1'b0,subE[7:0]} : (~{1'b0,subE[7:0]} + 9'b000000001);
+  assign temp1[8:0] = (one_gtthan_two) ? {1'b0,subE[7:0]} : ({1'b1,subE[7:0]});
 assign finalM[22:0] = (divout[24]) ? divout[23:1] : divout[22:0];
 assign temp[8:0] = (divout[24]) ? 9'b001111111 : 9'b001111110;
 assign {carryD,temp2[8:0]} = temp[8:0] + temp1[8:0];
